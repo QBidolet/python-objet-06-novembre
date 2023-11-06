e17ebdb16af298c6a40a682d6cf6a0fd83bbcdda
Quentin BIDOELT <quentin.bidolet@gmail.com>
2023-11-06 16:51:59 +0100

    - qcm énoncé - fin jour 1

--- exercices/exercice_1.py
+++ exercices/exercice_1.py
@@ -7,5 +7,14 @@ Indication pour la conversion :
     - miles/heure à m/s, il faut diviser par 2.237.
     - miles/heure à km/h, il faut multiplier par 1.609
 """
-reponse = input("Donnez une valeur.\n")
-print(reponse)
+liste_continuer = ["oui", "yes", "y", "o"]
+continuer = "oui"
+
+while continuer in liste_continuer:
+    saisie = input("Saisissez une vitesse en miles/heure.\n")
+    vitesse = float(saisie)
+    m_s = vitesse / 2.237
+    km_h = vitesse * 1.609
+    print(f"{km_h} km/h\n{m_s} m/s.")
+    continuer = input(f"\nVoulez-vous continuer ?({'/'.join(liste_continuer)})\n")
+    continuer = continuer.lower()
---
+++ exercices/qcm.py
@@ -0,0 +1,7 @@
+"""
+    Ecrire un QCM et afficher le score de l'utilisateur à la fin.
+    Créer une structure de données qui contiendra les questions (sous forme de chaine de caractère)
+    et les réponses.
+    Votre programme doit parcourir cette structure pour afficher les questions et vérifier si la réponse
+    est juste.
+"""
